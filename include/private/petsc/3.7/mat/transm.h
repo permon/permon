09c166be21c8124444caca1a13e1cdebea0ddc6c
Vaclav Hapla <hap014@vsb.cz>
2017-03-07 20:09:43 +0100

    petscimpl.h: Mat_Transpose moved to separate petsc-version-specific headerfile

---
+++ include/private/petsc/3.7/mat/transm.h
@@ -0,0 +1,9 @@
+
+#if !defined(__TRANSM_H)
+#define	__TRANSM_H
+
+typedef struct {
+  Mat A;
+} Mat_Transpose;
+
+#endif
--- include/private/petscimpl.h
+++ include/private/petscimpl.h
@@ -1,5 +1,5 @@
-#if !defined(__FLLOPMATSTRUCT_H)
-#define	__FLLOPMATSTRUCT_H
+#if !defined(__PETSCIMPL_H)
+#define	__PETSCIMPL_H
 
 #include "petscpc.h"
 
@@ -10,6 +10,7 @@
 #include "private/petsc/3.6/mat/normm.h"
 #include "private/petsc/3.6/mat/sbaij.h"
 #include "private/petsc/3.6/mat/shell.h"
+#include "private/petsc/3.6/mat/transm.h"
 #include "private/petsc/3.6/pc/redundant.h"
 #elif PETSC_VERSION_MINOR==7
 #include "private/petsc/3.7/mat/aij.h"
@@ -18,13 +19,10 @@
 #include "private/petsc/3.6/mat/normm.h"
 #include "private/petsc/3.7/mat/sbaij.h"
 #include "private/petsc/3.7/mat/shell.h"
+#include "private/petsc/3.7/mat/transm.h"
 #include "private/petsc/3.7/pc/redundant.h"
 #else
 #error "unsupported PETSc version"
 #endif
-
-typedef struct {
-  Mat A;
-} Mat_Transpose;
 
 #endif
