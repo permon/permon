bb7f6bd1f19a110c195fb86123a6d19e8abbdf7e
Jakub Kruzik <jakub.kruzik@gmail.com>
2018-10-03 15:38:58 +0200

    support PETSc 3.10 (3.9 still supported)

--- include/permonpetscretro.h
+++ include/permonpetscretro.h
@@ -2,8 +2,8 @@
 #define	__FLLOPPETSCRETRO_H
 #include <petscsys.h>
 
-#if PETSC_VERSION_MAJOR!=3 || (PETSC_VERSION_MAJOR==3 && PETSC_VERSION_MINOR!=9)
-#error "PERMON requires PETSc version 3.9"
+#if PETSC_VERSION_MAJOR!=3 || (PETSC_VERSION_MAJOR==3 && PETSC_VERSION_MINOR<9)
+#error "PERMON requires PETSc version 3.9 or higher"
 #endif
 
 /* allow to use petsc master branch while still supporting maint */
