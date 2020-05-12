747c51fabde082150255e42ba482eec4351b899c
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2020-05-12 23:02:16 +0200

    update mumpsimpl.h from PETSc mumps.c (#29)

--- include/permon/private/petsc/mat/mumpsimpl.h
+++ include/permon/private/petsc/mat/mumpsimpl.h
@@ -40,6 +40,12 @@ EXTERN_C_END
 #endif
 #endif
 
+/* MUMPS uses MUMPS_INT for nonzero indices such as irn/jcn, irn_loc/jcn_loc and uses int64_t for
+   number of nonzeros such as nnz, nnz_loc. We typedef MUMPS_INT to PetscMUMPSInt to follow the
+   naming convention in PetscMPIInt, PetscBLASInt etc.
+*/
+typedef MUMPS_INT PetscMUMPSInt;
+
 /* declare MumpsScalar */
 #if defined(PETSC_USE_COMPLEX)
 #if defined(PETSC_USE_REAL_SINGLE)
@@ -59,7 +65,8 @@ EXTERN_C_END
 #define RINFOG(I) rinfog[(I)-1]
 #define RINFO(I) rinfo[(I)-1]
 
-typedef struct {
+typedef struct Mat_MUMPS Mat_MUMPS;
+struct Mat_MUMPS {
 #if defined(PETSC_USE_COMPLEX)
 #if defined(PETSC_USE_REAL_SINGLE)
   CMUMPS_STRUC_C id;
@@ -74,27 +81,32 @@ typedef struct {
 #endif
 #endif
 
-  MatStructure matstruc;
-  PetscMPIInt  myid,size;
-  PetscInt     *irn,*jcn,nz,sym;
-  PetscScalar  *val;
-  MPI_Comm     comm_mumps;
-  PetscBool    isAIJ;
-  PetscInt     ICNTL9_pre;           /* check if ICNTL(9) is changed from previous MatSolve */
-  VecScatter   scat_rhs, scat_sol;   /* used by MatSolve() */
-  Vec          b_seq,x_seq;
-  PetscInt     ninfo,*info;          /* display INFO */
-  PetscInt     sizeredrhs;
-  PetscInt     *schur_pivots;
-  PetscInt     schur_B_lwork;
-  PetscScalar  *schur_work;
-  PetscScalar  *schur_sol;
-  PetscInt     schur_sizesol;
-  PetscBool    schur_factored;
-  PetscBool    schur_inverted;
+  MatStructure   matstruc;
+  PetscMPIInt    myid,petsc_size;
+  PetscMUMPSInt  *irn,*jcn;             /* the (i,j,v) triplets passed to mumps. */
+  PetscScalar    *val,*val_alloc;       /* For some matrices, we can directly access their data array without a buffer. For others, we need a buffer. So comes val_alloc. */
+  PetscInt64     nnz;                   /* number of nonzeros. The type is called selective 64-bit in mumps */
+  PetscMUMPSInt  sym;
+  MPI_Comm       mumps_comm;
+  PetscMUMPSInt  ICNTL9_pre;            /* check if ICNTL(9) is changed from previous MatSolve */
+  VecScatter     scat_rhs, scat_sol;    /* used by MatSolve() */
+  Vec            b_seq,x_seq;
+  PetscInt       ninfo,*info;           /* which INFO to display */
+  PetscInt       sizeredrhs;
+  PetscScalar    *schur_sol;
+  PetscInt       schur_sizesol;
+  PetscMUMPSInt  *ia_alloc,*ja_alloc;   /* work arrays used for the CSR struct for sparse rhs */
+  PetscInt64     cur_ilen,cur_jlen;     /* current len of ia_alloc[], ja_alloc[] */
+  PetscErrorCode (*ConvertToTriples)(Mat,PetscInt,MatReuse,Mat_MUMPS*);
 
-  PetscErrorCode (*Destroy)(Mat);
-  PetscErrorCode (*ConvertToTriples)(Mat, int, MatReuse, int*, int**, int**, PetscScalar**);
-} Mat_MUMPS;
+  /* stuff used by petsc/mumps OpenMP support*/
+  PetscBool      use_petsc_omp_support;
+  PetscOmpCtrl   omp_ctrl;              /* an OpenMP controler that blocked processes will release their CPU (MPI_Barrier does not have this guarantee) */
+  MPI_Comm       petsc_comm,omp_comm;   /* petsc_comm is petsc matrix's comm */
+  PetscInt64     *recvcount;            /* a collection of nnz on omp_master */
+  PetscMPIInt    tag,omp_comm_size;
+  PetscBool      is_omp_master;         /* is this rank the master of omp_comm */
+  MPI_Request    *reqs;
+};
 
 #endif
