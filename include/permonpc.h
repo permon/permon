fb4c00f6d02e27d8bf0d31bb6afc87e2a1cecfc7
Jakub Kruzik <jakub.kruzik@gmail.com>
2022-03-08 09:52:51 +0100

    dirichlet pc

--- include/permonpc.h
+++ include/permonpc.h
@@ -10,7 +10,7 @@ FLLOP_EXTERN PetscErrorCode FllopPCRegisterAll();
 FLLOP_EXTERN PetscBool FllopPCRegisterAllCalled;
 
 /* PCDUAL type-specific functions */
-typedef enum {PC_DUAL_NONE=0, PC_DUAL_LUMPED=1} PCDualType;
+typedef enum {PC_DUAL_NONE=0, PC_DUAL_LUMPED=1, PC_DUAL_DIRICHLET=2, PC_DUAL_DIRICHLET_DIAG=3, PC_DUAL_LUMPED_FULL=4} PCDualType;
 FLLOP_EXTERN const char *PCDualTypes[];
 FLLOP_EXTERN PetscErrorCode PCDualSetType(PC pc,PCDualType type);
 FLLOP_EXTERN PetscErrorCode PCDualGetType(PC pc,PCDualType *type);
